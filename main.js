2e2417bfdc285632e8c04ffb88772a6a6368bd4b
Jun <junaidvayoli@gmail.com>
2020-06-25 23:08:07 +0900

    refactor to use only one endpoint

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-var url = 'https://api.covid19india.org/v3/data-all.json';
 var url_timeseries = 'https://api.covid19india.org/v3/timeseries.json';
 var categories = [];
 var data_tt_confirmed = [];
@@ -13,25 +12,85 @@ var data_tt_active_delta = [];
 var data_tt_recovered_delta = [];
 var data_tt_deceased_delta = [];
 var data_tt_tested_delta = [];
+var annotations = {
+    xaxis: [
+        {
+            x: "2020-03-25",
+            strokeDashArray: 0,
+            borderColor: '#775DD0',
+            label: {
+                borderColor: '#775DD0',
+                // style: {
+                //     color: '#fff',
+                //     background: '#775DD0',
+                // },
+                text: 'Lockdown Phase 1',
+            }
+        },
+        {
+            x: "2020-04-15",
+            borderColor: '#775DD0',
+            label: {
+                borderColor: '#775DD0',
+                text: 'Lockdown Phase 2',
+            }
+        },
+        {
+            x: "2020-05-04",
+            borderColor: '#775DD0',
+            label: {
+                borderColor: '#775DD0',
+                text: 'Lockdown Phase 3',
+            }
+        },
+        {
+            x: "2020-05-18",
+            borderColor: '#775DD0',
+            label: {
+                borderColor: '#775DD0',
+                text: 'Lockdown Phase 4',
+            }
+        },
+        {
+            x: "2020-06-01",
+            borderColor: '#775DD0',
+            label: {
+                borderColor: '#775DD0',
+                text: 'Lockdown Phase 5',
+            }
+        }
+    ],
+
+};
 
 
 fetch(url_timeseries)
     .then(res => res.json())
     .then((data) => {
-        console.log(data["TT"]);
+
         for (var date in data["TT"]) {
             console.log(date);
             categories.push(date);
-            
+
             data_tt_confirmed_delta.push(data["TT"][date].delta.confirmed || 0);
             data_tt_recovered_delta.push(data["TT"][date].delta.recovered || 0);
             data_tt_deceased_delta.push(data["TT"][date].delta.deceased || 0);
             data_tt_tested_delta.push(data["TT"][date].delta.tested || 0);
             // data_tt_active_delta.push(data["TT"][date].delta.confirmed || 0);
+
+            data_tt_confirmed.push(data["TT"][date].total.confirmed || 0);
+            data_tt_recovered.push(data["TT"][date].total.recovered || 0);
+            data_tt_deceased.push(data["TT"][date].total.deceased || 0);
+            data_tt_tested.push(data["TT"][date].total.tested || 0);
+            data_tt_active.push(data["TT"][date].total.confirmed
+                - (data["TT"][date].total.recovered || 0)
+                - (data["TT"][date].total.deceased || 0)
+            );
+
         }
 
 
-        var options = {
+        var options_daily = {
             chart: {
                 type: 'line',
                 // zoom:{
@@ -39,54 +98,28 @@ fetch(url_timeseries)
                 // },
             },
             annotations: {
-                xaxis: [
-                    {
-                        x: "2020-03-25",
-                        strokeDashArray: 0,
-                        borderColor: '#775DD0',
-                        label: {
-                            borderColor: '#775DD0',
-                            // style: {
-                            //     color: '#fff',
-                            //     background: '#775DD0',
-                            // },
-                            text: 'Lockdown Phase 1',
-                        }
+                xaxis: annotations.xaxis,
+                points: [{
+                    x: '2020-06-16',
+                    y: 2004,
+                    marker: {
+                        size: 8,
+                        fillColor: '#fff',
+                        strokeColor: 'red',
+                        radius: 2,
+                        cssClass: 'apexcharts-custom-class'
                     },
-                    {
-                        x: "2020-04-15",
-                        borderColor: '#775DD0',
-                        label: {
-                            borderColor: '#775DD0',
-                            text: 'Lockdown Phase 2',
-                        }
-                    },
-                    {
-                        x: "2020-05-04",
-                        borderColor: '#775DD0',
-                        label: {
-                            borderColor: '#775DD0',
-                            text: 'Lockdown Phase 3',
-                        }
-                    },
-                    {
-                        x: "2020-05-18",
-                        borderColor: '#775DD0',
-                        label: {
-                            borderColor: '#775DD0',
-                            text: 'Lockdown Phase 4',
-                        }
-                    },
-                    {
-                        x: "2020-06-01",
-                        borderColor: '#775DD0',
-                        label: {
-                            borderColor: '#775DD0',
-                            text: 'Lockdown Phase 5',
-                        }
-                    }
-                ],
+                    label: {
+                        borderColor: '#FF4560',
+                        offsetY: 0,
+                        style: {
+                            color: '#fff',
+                            background: '#FF4560',
+                        },
 
+                        text: 'Backdated deaths reported by MH',
+                    }
+                }]
             },
             colors: ['#ff073a', '#28a745', '#6c757d', '#261ebd'],
             series: [
@@ -112,87 +145,14 @@ fetch(url_timeseries)
             }
         }
 
-        var chart = new ApexCharts(document.querySelector("#chart-daily"), options);
-
-        chart.render();
-
-    })
-    .catch(err => { throw err });
-
-fetch(url)
-    .then(res => res.json())
-    .then((data) => {
-        for (var date in data) {
-            categories.push(date);
-            data_tt_confirmed.push(data[date]['TT'].total.confirmed);
-            data_tt_recovered.push(data[date]['TT'].total.recovered || 0);
-            data_tt_deceased.push(data[date]['TT'].total.deceased || 0);
-            data_tt_tested.push(data[date]['TT'].total.tested || 0);
-            data_tt_active.push(data[date]['TT'].total.confirmed
-                - (data[date]['TT'].total.recovered || 0)
-                - (data[date]['TT'].total.deceased || 0)
-            );
-        }
-
-        console.log(data_tt_confirmed.length);
-        console.log(data_tt_recovered.length);
-        var options = {
+        var options_cumulative = {
             chart: {
                 type: 'line',
                 // zoom:{
                 //     type: 'xy'
                 // },
             },
-            annotations: {
-                xaxis: [
-                    {
-                        x: "2020-03-25",
-                        strokeDashArray: 0,
-                        borderColor: '#775DD0',
-                        label: {
-                            borderColor: '#775DD0',
-                            // style: {
-                            //     color: '#fff',
-                            //     background: '#775DD0',
-                            // },
-                            text: 'Lockdown Phase 1',
-                        }
-                    },
-                    {
-                        x: "2020-04-15",
-                        borderColor: '#775DD0',
-                        label: {
-                            borderColor: '#775DD0',
-                            text: 'Lockdown Phase 2',
-                        }
-                    },
-                    {
-                        x: "2020-05-04",
-                        borderColor: '#775DD0',
-                        label: {
-                            borderColor: '#775DD0',
-                            text: 'Lockdown Phase 3',
-                        }
-                    },
-                    {
-                        x: "2020-05-18",
-                        borderColor: '#775DD0',
-                        label: {
-                            borderColor: '#775DD0',
-                            text: 'Lockdown Phase 4',
-                        }
-                    },
-                    {
-                        x: "2020-06-01",
-                        borderColor: '#775DD0',
-                        label: {
-                            borderColor: '#775DD0',
-                            text: 'Lockdown Phase 5',
-                        }
-                    }
-                ],
-
-            },
+            annotations: annotations,
             colors: ['#ff073a', '#007bff', '#28a745', '#6c757d', '#261ebd'],
             series: [
                 {
@@ -222,9 +182,12 @@ fetch(url)
             }
         }
 
-        var chart = new ApexCharts(document.querySelector("#chart-cumulative"), options);
+        var chart_cumulative = new ApexCharts(document.querySelector("#chart-cumulative"), options_cumulative);
+        chart_cumulative.render();
 
-        chart.render();
+
+        var chart_daily = new ApexCharts(document.querySelector("#chart-daily"), options_daily);
+        chart_daily.render();
 
     })
     .catch(err => { throw err });
